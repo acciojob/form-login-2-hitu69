80efb9780db0a13d84f628c9f5f58a41a1b673d7
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-04-08 12:22:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function showData() {
+let myArr = [];
+myArr[0] = "First Name: " + document.getElementById('fname').value;
+myArr[1] = "Last Name: " +document.getElementById('lname').value;
+myArr[2] = "Phone Number: " + document.getElementById('number').value;
+myArr[3] = "Email ID: " + document.getElementById('email').value;
+var text = myArr[0] + "\n" + myArr[1]
++ "\n" + myArr[2]
++ "\n" + myArr[3]
+alert(text)
+}
